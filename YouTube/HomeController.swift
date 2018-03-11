af434a25e05a07caf046a29333274ab07d745122
Shaun Tucker <tucke@shauns-air.home>
2018-03-11 14:06:44 -0400

    SeperatorView added

--- YouTube/HomeController.swift
+++ YouTube/HomeController.swift
@@ -60,12 +60,26 @@ class VideoCell: UICollectionViewCell {
         return imageView
     }()
     
+    let seperatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black
+        view.translatesAutoresizingMaskIntoConstraints = false 
+        return view
+    }()
+    
     func setupViews() {
         addSubview(thumbnailImageView)
+        addSubview(seperatorView)
         
+        //constraints for my thumbnail images
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": thumbnailImageView]))
         
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": thumbnailImageView]))
+        
+        //constraints for my seperator view
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": seperatorView]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[v0(1)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": seperatorView]))
         
         
         
