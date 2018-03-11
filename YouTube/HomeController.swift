373b3c475ce972b57adfcc53324964bdd39132dc
Shaun Tucker <tucke@shauns-air.home>
2018-03-11 11:53:27 -0400

    YouTube initial commit

---
+++ YouTube/HomeController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Shaun Tucker on 3/11/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+    }
+
+
+
+}
+
