6f07efcccb89e29d3c1654687db2b2b3de33b915
Shaun Tucker <tucke@shauns-air.home>
2018-03-11 12:09:15 -0400

    CollectionCell added

--- YouTube/HomeController.swift
+++ YouTube/HomeController.swift
@@ -9,14 +9,32 @@
 import UIKit
 
 class HomeController: UICollectionViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.red
+        navigationItem.title = "Home"
+        
+        collectionView?.backgroundColor = UIColor.white
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        
+        
     }
 
-
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        
+        cell.backgroundColor = UIColor.red
+        
+        return cell
+    }
 }
 
