6128e0b5bb0f054a91fd14ebe8c6a034502149e5
Shaun Tucker <tucke@shauns-air.home>
2018-03-12 22:10:40 -0400

    MenuBar added

---
+++ YouTube/View/MenuBar.swift
@@ -0,0 +1,21 @@
+//
+//  MenuBar.swift
+//  YouTube
+//
+//  Created by Shaun Tucker on 3/12/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+class MenuBar: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
